5e83b54687589ae76c15bb8023de608cde2bf86a
akhilkokani <akhilkokani@gmail.com>
2019-12-25 20:55:37 +0530

    Solved Problem Statement.

---
+++ src/viral_advertising.py
@@ -0,0 +1,30 @@
+# Problem Statement Link: https://www.hackerrank.com/challenges/strange-advertising/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the viralAdvertising function below.
+def viralAdvertising(n):
+    shared = 5
+    cumulative = 0
+    for i in range(n):
+        liked = int(shared/2)
+        cumulative += liked
+        shared = liked * 3
+    return cumulative
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
