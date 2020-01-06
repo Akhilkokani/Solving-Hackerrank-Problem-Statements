ccce678a63db1dfd88bc676c17e4f8b501bd3bbf
Akhil Kokani <akhilkokani@gmail.com>
2020-01-06 21:19:02 +0530

    Solved Problem Statement.

---
+++ 2020/repeated_string.py
@@ -0,0 +1,26 @@
+# Problem Statement Link: https://www.hackerrank.com/challenges/repeated-string/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the repeatedString function below.
+def repeatedString(s, n):
+    return s.count('a') * int(n/len(s)) + s[:(n%len(s))].count('a')
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
