6918f381f99f9f98af13f2e248853a25d4d7e496
Akhil Kokani <akhilkokani@gmail.com>
2020-01-15 21:57:30 +0530

    Solved Problem Statement.

--- 2020/beautiful_triplets.py
+++ 2020/beautiful_triplets.py
@@ -10,13 +10,8 @@ import sys
 def beautifulTriplets(d, arr):
     bt = 0
     for i in arr:
-        extra = 0
-        if i > 0 and (abs(i-d) in arr) and (abs(i-d)-d in arr):
-            if arr.count(i) > 1: extra = arr.count(i)
-            if arr.count(abs(i-d)) > 1: extra += arr.count(abs(i-d))
-            if arr.count(abs(i-d)-d) > 1: extra += arr.count(abs(i-d)-d)
-            if extra > 0: bt += extra
-            else: bt += 1
+        if (i+d) in arr and (i+d)+d in arr:
+            bt += 1
     return bt
 
 if __name__ == '__main__':
