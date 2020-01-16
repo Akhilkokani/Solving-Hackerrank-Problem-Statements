a5d8df93cfd3c669a3a9d8014ea4547b4b830582
Akhil Kokani <akhilkokani@gmail.com>
2020-01-16 12:51:56 +0530

    Solved Problem Statement.

---
+++ 2020/organizing_containers_of_balls.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        n = int(input())
+
+        m = []
+        rowsum = [0]*n
+        colsum = [0]*n
+
+        for _ in range(n):
+            m.append(list(map(int, input().rstrip().split())))
+
+            rowsum[ _ ] = sum(m[-1])    # Logic Start
+            colsum = list(map(lambda a, b: a+b, colsum, m[-1]))
+        
+        rowsum.sort()
+        colsum.sort()
+        
+        if rowsum == colsum:
+            fptr.write("Possible" + '\n')
+        else:
+            fptr.write("Impossible" + '\n')     # Logic End
+
+    fptr.close()
