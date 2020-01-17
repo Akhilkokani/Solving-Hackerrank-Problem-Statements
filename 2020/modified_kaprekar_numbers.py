7c7102ec046cde7b4d294858c715bd9e348dcffd
Akhil Kokani <akhilkokani@gmail.com>
2020-01-17 19:41:35 +0530

    Solved Problem Statement.

---
+++ 2020/modified_kaprekar_numbers.py
@@ -0,0 +1,32 @@
+# Problem Statement Link: https://www.hackerrank.com/challenges/kaprekar-numbers/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the kaprekarNumbers function below.
+def kaprekarNumbers(p, q):
+    kn = ""
+    for i in range(p, q+1):
+        s = str(i * i)
+        if len(s) > 1:
+            h = len(s)//2
+            if ( int(s[:h]) + int(s[h:]) ) == i:
+                kn += str( int(s[:h]) + int(s[h:]) ) + " "
+        elif int(s) == i:
+                kn += s + " "
+    
+    if kn: print(kn)
+    else: print("INVALID RANGE")
+
+
+if __name__ == '__main__':
+    p = int(input())
+
+    q = int(input())
+
+    kaprekarNumbers(p, q)
