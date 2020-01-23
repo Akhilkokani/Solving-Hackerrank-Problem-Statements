6337ee6f4657fd50b5429f41f64ebdb16babe110
Akhil Kokani <akhilkokani@gmail.com>
2020-01-23 20:53:24 +0530

    Solved Problem Statement.

---
+++ 2020/print_function.py
@@ -0,0 +1,7 @@
+# Problem Statement Link: https://www.hackerrank.com/challenges/python-print/problem
+
+if __name__ == '__main__':
+    s = ""
+    for i in range( 1, int(input())+1 ):
+        s += str(i)
+    print(s)
