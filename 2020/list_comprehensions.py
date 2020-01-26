dd99df99dc7f072065b01823a196834f11ce2c3a
Akhil Kokani <akhilkokani@gmail.com>
2020-01-26 06:51:26 +0530

    Solved Problem Statement.

--- 2020/list_comprehensions.py
+++ 2020/list_comprehensions.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
     ar = []
     p = 0
 
-    for a in range(0,x+1): 
-        for b in range(0,y+1): 
-            for c in range(0,z+1):
-                 if a + b + c != n:
-                     ar.append( [a,b,c] )
-    print(ar)
+    ar = [[a,b,c] for a in range(0,x+1) \
+                    for b in range(0,y+1) \
+                        for c in range(0,z+1) \
+                            if a+b+c != n
+         ]
+    print( ar )
 
